a103416521ac6574c71bf8c10aaea79d5c4bc41a
PETEROBYNO <peterobinna67@gmail.com>
2023-10-09 07:26:27 -0700

    100-intel

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- * main	- A program that print the new line using printf function
- * Return: 0(success)
+ * main	- A program that Print "with proper grammar, but the outcome is a piece of art," , followed by a new line.
+ * Return: 0(Success)
  */
 int main(void)
-printf("with proper grammar, but the outcome is a piece of art,\n");
+{
+printf("with proper graimmar, but the outcome is a piece of art,\n")
 return (0);
 }

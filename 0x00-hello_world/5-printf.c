7f65f75e9b96e4864922a8011ec55e018c8bbaf3
PETEROBYNO <peterobinna67@gmail.com>
2023-10-09 11:26:38 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main	- A program that Print a line using printf function
+ * main	- A program that Print a new  line using printf function
  * Return: 0(Success)
  */
 int main(void)
 {
-iprintf("With proper graimmar, but the outcome is a piece of art,\n");
+printf("With proper graimmar, but the outcome is a piece of art,\n");
 return (0);
 }

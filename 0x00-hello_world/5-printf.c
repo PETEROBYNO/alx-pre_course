128ff9e3753182fb96d6ce51ae0ef69c2d2c4a22
PETEROBYNO <peterobinna67@gmail.com>
2023-10-09 05:39:27 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main	- A program that print the line using printf function
+ * main	- A program that print the new line using printf function
  * Return: 0(success)
  */
 int main(void)
-printf("Thi is a piece of art,\n")
+printf("with proper grammar, but the outcome is a piece of art, \n")
 return (0);
 }

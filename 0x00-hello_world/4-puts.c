55a23acd5bf9c8746294a4665418664e27a245be
PETEROBYNO <peterobinna67@gmail.com>
2023-10-09 03:29:15 -0700

    2-assembler

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/**
+ * main - A program that print a line with puts function
+ * Return: 0 (succes)
+int main (void)
+{
+puts ("1"Programming is like building a multilingual puzzle");
+Return (0);

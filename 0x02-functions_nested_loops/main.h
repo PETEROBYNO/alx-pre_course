56ea9e133a5df799efd269a51f929cc5a285197a
PETEROBYNO <peterobinna67@gmail.com>
2023-10-11 22:21:55 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,7 +2,6 @@
 #define MAIN_HEADER_FILE
 
 int _putcar(char.c);
-
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -15,4 +14,4 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 
-#endif
+#endif 

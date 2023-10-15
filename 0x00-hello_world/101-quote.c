c73bf9c73b7e1db30c5e93d71da4d2c826b04ea5
PETEROBYNO <peterobinna67@gmail.com>
2023-10-14 23:26:57 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,12 @@
 #include <unistd.h>
 
+/**
+ * main	- print "and that piece of art is useful" -Dora Korpar, 2015-10-19
+ * followed by a new line, to the standard error.
+ * Return: Always 1.
+ */
+
 int main(void)
 {
-    char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    ssize_t len = 0;
-
-    while (message[len] != '\0') {
-        len++;
-    }
-
-    write(2, message, len);
-
-    return 1;
+write(2, "and that pieace of art is useful\" -Door kopar,2015-10119\n", 59);
 }
